f75fbc79d2cbf2d140fda6d8b02fa6886402e869
SamReha <sreha@ucsc.edu>
2015-05-03 15:07:28 -0700

    Did a bunch of work today

--- p5.py
+++ p5.py
@@ -4,13 +4,66 @@ from collections import namedtuple
 
 import planner as Plan
 
+# Helper functions!
+def graph(state):
+    for r in all_recipes:
+        if r.check(state):
+            yield (r.name, r.effect(state), r.cost)
+            
+def is_goal(state):
+    goal_state = make_state(goal, items)
+    for i in range(0, len(goal_state)):
+        if state[i] < goal_state[i]:
+            return False
+    return True
+    
+def heuristic(state, goal_state):
+    return 0
+    
+def make_state(inventory, item_set):
+    state = []
+    
+    for i in range(0, len(item_set)):
+        if item_set[i] in inventory:
+            state.append(inventory[item_set[i]])
+        else:
+            state.append(0)
+    
+    return tuple(state)
+    
+def get_item_dictionary(item_set):
+    itemDict = dict()
+    for i in range(0, len(item_set)):
+        itemDict[item_set[i]] = i
+        
+    return itemDict
+
 # Prototypes for make_checker and make_effector. These will be used in the process of 
 # converting the recipes into a more efficient format.
 def make_checker(rule):
     # this code runs once
     # do something with rule['Consumes'] and rule['Requires']
+    try:
+        requirements = rule['Requires'].copy()
+    except KeyError:
+        requirements = dict()
+    
+    try:
+        consumes = rule['Consumes'].copy()
+    except KeyError:
+        consumes = dict()
+    
+    for key in requirements:
+        requirements[key] = 1
+        
+    consumes.update(requirements)
     def check(state):
         # this code runs millions of times
+        for item in consumes:
+            indexOfItem = itemDict[item]
+            if consumes[item] > state[indexOfItem]:
+                return False
+        
         return True # or False
     
     return check
@@ -18,9 +71,28 @@ def make_checker(rule):
 def make_effector(rule):
     # this code runs once
     # do something with rule['Produces'] and rule['Consumes']
+    toAdd = {}
+    toRemove = {}
+    
+    for item in rule['Produces']:
+        toAdd[item] = rule['Produces'][item]
+            
+    try:
+        for item in rule['Consumes']:
+            toRemove[item] = rule['Consumes'][item]
+    except KeyError:
+        toRemove = {}
+        
     def effect(state):
         # this code runs millions of times
-        return next_state
+        next_state = list(state)
+        for item in toAdd:
+            next_state[itemDict[item]] += toAdd[item]
+            
+        for item in toRemove:
+            next_state[itemDict[item]] -= toRemove[item]
+        
+        return tuple(next_state)
     
     return effect
 
@@ -37,6 +109,8 @@ except IOError:
     
 items, inventory, goal = Crafting['Items'], Crafting['Initial'], Crafting['Goal']
 
+itemDict = get_item_dictionary(items)
+
 # Convert recipes into more efficient format
 Recipe = namedtuple('Recipe',['name','check','effect','cost'])
 all_recipes = []
@@ -45,11 +119,14 @@ for name, rule in Crafting['Recipes'].items():
     effector = make_effector(rule)
     recipe = Recipe(name, checker, effector, rule['Time'])
     all_recipes.append(recipe)
+    
+initial_state = make_state(inventory, items)
 
-plan = Plan.get_plan(items, inventory, goal, all_recipes)
+cost, plan = Plan.search(graph, initial_state, is_goal, 10000, heuristic)
 
 # Display plan
 if plan is None:
     print "No plan could be found!"
 else:
+    print "Total Cost: " + str(cost)
     print plan
--- planner.py
+++ planner.py
@@ -1,2 +1,46 @@
-def get_plan(item_set, inventory, goal_state, recipe_set):
-    return None
+from math import sqrt
+from heapq import heappush, heappop
+
+def search(graph, initial, is_goal, limit, heuristic):
+    total_cost = 0
+    plan = None
+    
+    cost = {}
+    prev = {}
+    verbose = {}
+    q = []
+
+    cost[initial] = 0
+    prev[initial] = None
+    verbose[initial] = ("no_action", initial, 0)
+    heappush(q, (cost[initial], initial))
+
+    while len(q) > 0:
+        _, u = heappop(q)
+
+        if is_goal(u):
+            break
+
+        neighborhood = graph(u)
+
+        for neighbor in neighborhood:
+            name, effect, neighborCost = neighbor
+            alt = cost[u] + neighborCost
+            if effect not in cost or alt < cost[effect]:
+                cost[effect] = alt
+                prev[effect] = u
+                verbose[effect] = neighbor
+                heappush(q, (alt, effect))
+
+    if is_goal(u):
+        plan = []
+        total_cost = cost[u]
+        while u:
+            plan.append(verbose[u])
+            u = prev[u]
+        plan.reverse()
+        return total_cost, plan
+    else:
+        return 0, []
+
+    return total_cost, plan
